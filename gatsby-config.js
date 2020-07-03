99f426428b6cff883c4fff875ef522a6149db65d
Jonathan Martinez <jonathan@MacBook-Pro-de-Jonathan.local>
2020-07-03 21:09:46 +0200

    first version seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Jommartinez - Frontend & WordPress developer`,
@@ -28,8 +32,57 @@ module.exports = {
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `http://localhost:8000/`,
+        stripQueryString: true,
+      },
+    },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: process.env.API_URL,
+        protocol: "https",
+        restApiRoutePrefix: "wp-json",
+        hostingWPCOM: false,
+        useACF: true,
+        acfOptionPageIds: [],
+        cookies: {},
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "https://source-url.com",
+          replacementUrl: "https://replacement-url.com",
+        },
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+          "**/ai_proyectos",
+          "**/ai_categorias",
+        ],
+        excludedRoutes: ["**/posts/1456"],
+        keepMediaSizes: false,
+        normalizer: function ({ entities }) {
+          return entities
+        },
+        normalizers: normalizers => [
+          ...normalizers,
+          {
+            name: "nameOfTheFunction",
+            normalizer: function ({ entities }) {
+              // manipulate entities here
+              return entities
+            },
+          },
+        ],
+      },
+    },
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Titulo de la home" description="descripcion de la página" />
     <h1>Hi people</h1>
+    <h2>{`${process.env.GATSBY_API_URL}`}</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
