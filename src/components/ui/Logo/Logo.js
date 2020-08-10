b0676d801476283daead7e72ee89a5c2d5e379e5
Jonathan Martinez <jonathan@MacBook-Pro-de-Jonathan.local>
2020-08-10 08:55:15 +0200

    add svgs and fixed local font

--- src/components/ui/Logo/Logo.js
+++ src/components/ui/Logo/Logo.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { LogoStyled } from "./LogoStyle"
+import LogoSvg from "../../svg/LogoSvg"
 
 function Logo() {
   return (
     <LogoStyled>
-      <div>Logo</div>
+      <LogoSvg />
     </LogoStyled>
   )
 }
