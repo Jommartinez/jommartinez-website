c7d23ff26e2384c66fbec4d4e06bebdd0a938407
Jonathan Martinez <jonathan@MacBook-Pro-de-Jonathan.local>
2020-08-29 19:22:58 +0200

    fixed relative path

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,13 @@
-import React, { useRef, useEffect } from "react"
-import { gsap } from "gsap"
+import React from "react"
 import RightHeader from "../ui/RightHeader"
 import Logo from "../ui/Logo"
 import { HeaderStyled } from "./HeaderStyle"
 
 function Header({ setDarkMode, darkMode }) {
-  const headerRef = useRef(null)
-
-  useEffect(() => {
-    gsap.from(headerRef.current, {
-      y: 10,
-      opacity: 0,
-      duration: 0.35,
-      delay: 0,
-    })
-  }, [])
-
   return (
     <HeaderStyled>
       <div className="wrapper">
-        <div className="content" ref={headerRef}>
+        <div className="content">
           <Logo />
           <RightHeader setDarkMode={setDarkMode} darkMode={darkMode} />
         </div>
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -7,11 +7,7 @@ const Project = ({ proyecto }) => {
   const { slug, titulo } = proyecto
   return (
     <ProjectStyled>
-      <AniLink
-        paintDrip
-        hex="#f5b133"
-        to={`https://distracted-fermat-b7befd.netlify.app/${slug}`}
-      >
+      <AniLink paintDrip hex="#f5b133" to={`/${slug}`}>
         <img src={imageProject} alt={slug} />
         <div className="cover">
           <div className="mask"></div>
