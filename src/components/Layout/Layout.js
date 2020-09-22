0cd3047baa864701311c52388d63d852a9d57aec
Jonathan Martinez <jonathan@MacBook-Pro-de-Jonathan.local>
2020-09-22 16:28:20 +0200

    fix darkmode

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -14,21 +14,25 @@ export default function Layout({ children }) {
   }, [darkMode])
 
   function getInitialMode() {
-    const isReturningUser = "dark" in localStorage
-    const savedMode = JSON.parse(localStorage.getItem("dark"))
-    const userPrefersDark = getPrefColorScheme()
+    if (window) {
+      const isReturningUser = "dark" in localStorage
+      const savedMode = JSON.parse(localStorage.getItem("dark"))
+      const userPrefersDark = getPrefColorScheme()
 
-    // if mode was saved --> dark / light
-    if (isReturningUser) {
-      return savedMode
-      // if preferred color scheme is dark --> dark
-    } else if (userPrefersDark) {
-      return true
-      // otherwise --> light
+      // if mode was saved --> dark / light
+      if (isReturningUser) {
+        return savedMode
+        // if preferred color scheme is dark --> dark
+      } else if (userPrefersDark) {
+        return true
+        // otherwise --> light
+      } else {
+        return false
+      }
+      // return savedMode || false;
     } else {
-      return false
+      return true
     }
-    // return savedMode || false;
   }
 
   function getPrefColorScheme() {
